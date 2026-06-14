116d65778f6207b6a69daed82b65ea2fa354dbf4
Lewis Cushnie <lewiscushnie@icloud.com>
2026-06-14 18:01:47 +0100

    updated name typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ fi
 
 run_deploy() {
   local label="$1"
-  mkdir -p /home/lewis/homelab/data/WorthIt/postgres
+  mkdir -p /home/lewis/homelab/data/WorthIT/postgres
   echo "Starting containers (${label})..."
   docker compose "${COMPOSE_ENV[@]}" "${COMPOSE_FILES[@]}" up -d --build
 
